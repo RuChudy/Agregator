937a67cf65d98e1cf344a515fcc1cba87835dd07
Rudolf Chudý Lic. <ruchudy@gmail.com>
2024-09-29 18:16:42 +0200

    Add Tests

---
+++ Test/OrdersWebApiTest.cs
@@ -0,0 +1,39 @@
+﻿namespace Ordering.UnitTests;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using Agregator.API.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Agregator.API;
+
+[TestClass]
+public class OrdersWebApiTest
+{
+    private readonly ILogger<AgregatorService> _loggerMock;
+
+    public OrdersWebApiTest()
+    {
+        _loggerMock = Substitute.For<ILogger<AgregatorService>>();
+    }
+
+    [TestMethod]
+    public async Task Insert_orders_ok_request()
+    {
+        var data = new Order[]
+        {
+            new Order("a", 1.0M),
+            new Order("b", 2.0M)
+        };
+
+        // Act
+        var agregatorServices = new AgregatorService(_loggerMock);
+        var result = await AgregatorApi.CreateOrderAsync(data.ToList(), agregatorServices);
+
+        // Assert
+        Assert.IsInstanceOfType(result.Result, typeof(Ok));
+    }
+}
